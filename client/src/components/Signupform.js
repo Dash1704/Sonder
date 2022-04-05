353a81d51c197f2e26b05dae0973bf3b2f7ac13c
Francesco <francesco@monzo.com>
2022-04-05 13:26:40 +0200

    Added functionality to the signup form

--- client/src/components/Signupform.js
+++ client/src/components/Signupform.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const SignupForm = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const postSignup = () => {
+        fetch("https://localhost:8080/users", {
+            method: "post",
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err))
+    }
+
     return (
 
     <div>
         <form>
             <label htmlFor="email">Email</label>
-            <input type="text" /> 
+            <input type="text" 
+                value={email}
+                onChange = {(e) => setEmail(e.target.value)}
+            /> 
 
-            <label htmlFor="email">Password</label>
-            <input type="text" />
+            <label htmlFor="password">Password</label>
+            <input type="text" 
+                value={password}
+                onChange = {(e) => setPassword(e.target.value)}
+            />
 
-            <input type="submit"/>
+            <input type="submit"
+                onClick = {() => postSignup()} 
+            />
     </form>
     </div>
 
