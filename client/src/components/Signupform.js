3f6904ad097fc495fa8363b0b0fb72aa10d6542a
Chris Middleton <ctjmiddleton@gmail.com>
2022-04-05 11:36:26 +0100

    Added form to signup form element

--- client/src/components/Signupform.js
+++ client/src/components/Signupform.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 const SignupForm = () => {
     return (
-    <form>
-        
 
+    <div>
+        <form>
+            <label htmlFor="email">Email</label>
+            <input type="text" /> 
+
+            <label htmlFor="email">Password</label>
+            <input type="text" />
+
+            <input type="submit"/>
     </form>
+    </div>
+
     )
   }
 
