9ea175eee8322d014f26bb0000e85a76d1a64a0f
Chris Middleton <ctjmiddleton@gmail.com>
2022-04-06 15:07:11 +0100

    Fetching all databased requests

--- client/src/screens/RequestPage.js
+++ client/src/screens/RequestPage.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import AddRequest from '../components/AddRequest/AddRequest.js'
 import ListRequest from '../components/ListRequest/ListRequest.js'
 
 const RequestPage = () => {
+
+    const [allRequests, setAllRequests] = useState([])
+
+    useEffect(()=>{
+        fetch("/requests",{
+          headers:{
+            'Content-Type':'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(result => {
+            setAllRequests(result.posts)
+            })
+        }, [])
+
     return (
       <>
       <h1>Request Page</h1>
-      < AddRequest />
+      < AddRequest 
+        setAllRequests={setAllRequests}
+        allRequests={allRequests}/>
       < ListRequest />
       </>
     )
