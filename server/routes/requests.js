4b1609cf424784e3d6891d9a6e12ea15ccb2a5d6
Eddie Arnold <eddiearnoldz@me.com>
2022-04-06 15:26:38 +0100

    changed request route to view all requests to /requests

--- server/routes/requests.js
+++ server/routes/requests.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const RequestController = require("../controllers/requests");
 
 router.post("/new", RequestController.Create)
-router.get('/view', RequestController.View)
+router.get('/', RequestController.View)
 
 module.exports = router
