ac4f6b2d38e67f47ff04047f4a6e9b679aab20ab
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-04-06 16:50:42 +0100

    Updated the sign up cypress integration test with the correct syntax to connect wiht the testid portion of the code. co-authored by:now-what<now-what@users.noreply.github.com>”

--- server/cypress/integration/user_can_sign_up.test.js
+++ server/cypress/integration/user_can_sign_up.test.js
@@ -1,11 +1,12 @@
-describe("Signup", () => {
+  describe("Signup", () => {
     it("A user signs up and is redirected to sign in", () => {
       // sign up
       cy.visit("http://localhost:3000/signup");
-      cy.get("#Name").type("someone");
-      cy.get("#Email").type("someone@someone.com");
-      cy.get("#Password").type("password");
-      cy.get("#Submit").click();
+      
+      cy.get('input[data-testid="name"]').type("someone");
+      cy.get('input[data-testid="email"]').type("someone@test.com");
+      cy.get('input[data-testid="password"]').type("1234");
+      cy.get('input[data-testid="signup-button"]').click();
   
       cy.url().should("include", "/posts");
     });
