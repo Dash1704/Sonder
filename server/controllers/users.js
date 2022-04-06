8a9ac026fe35b2ffe6e2edd6d88226d789b4514d
Chris Middleton <ctjmiddleton@gmail.com>
2022-04-06 11:24:30 +0100

    can create user in database

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -26,17 +26,17 @@ const UsersController = {
       password: hashedPassword
     })
     if(user) {
-      res.status(201).json({
+      return res.status(201).json({
         _id: user.id,
         name: user.name,
         email: user.email
-      })
+      }, {message: 'User registered'})
     } else {
       res.status(400)
       throw new Error ('invalid user data')
     }
   
-    res.status(200).json({message: 'User registered'})
+    // return res.status(200).json({message: 'User registered'})
   })
 }
 
