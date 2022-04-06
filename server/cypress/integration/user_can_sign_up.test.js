5bec73f1ed26c51b9f35bb961e321c27d013ba35
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-04-06 16:10:42 +0100

    Updated the cy.visit URL page for the user_can_sign_up.test.js cypress test.

--- server/cypress/integration/user_can_sign_up.test.js
+++ server/cypress/integration/user_can_sign_up.test.js
@@ -1,11 +1,11 @@
 describe("Signup", () => {
     it("A user signs up and is redirected to sign in", () => {
       // sign up
-      cy.visit("/signup");
-      cy.get("#name").type("someone");
-      cy.get("#email").type("someone@someone.com");
-      cy.get("#password").type("password");
-      cy.get("#submit").click();
+      cy.visit("http://localhost:3000/signup");
+      cy.get("#Name").type("someone");
+      cy.get("#Email").type("someone@someone.com");
+      cy.get("#Password").type("password");
+      cy.get("#Submit").click();
   
       cy.url().should("include", "/posts");
     });
