e3ffe7f805cc51293a2c7278408151596d11f204
Eddie Arnold <eddiearnoldz@me.com>
2022-04-07 16:06:14 +0100

    home page and sign up routes connected
    Co-authored by:Jaroslaw-Roman-Cudzich<Jaroslaw-Roman-Cudzich@users.noreply.github.com>
    Co-authored by:Dash1704<Dash1704@users.noreply.github.com>
    Co-authored by:ctjmid<ctjmid@users.noreply.github.com>

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 
+//importing components
+import HomePage from './screens/HomePage.js';
 import Login from './screens/Login.js';
+import AboutUs from './screens/AboutUs.js';
 import SignUpDonorPage from './screens/SignUpDonorPage.js';
 import SignUpMotherPage from './screens/SignUpMotherPage.js';
 import RequestPage from './screens/RequestPage.js';
-import HomePage from './screens/HomePage.js';
 import NavBar from './components/NavBar';
 
 function App() {
@@ -14,11 +16,13 @@ function App() {
     <BrowserRouter>
     <NavBar/>
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup/donor" element={<SignUpDonorPage />} />
         <Route path="/signup/mother" element={<SignUpMotherPage />} />
         <Route path="/requests" element={<RequestPage />} />
-        <Route path="/" element={<HomePage />} />
+   
       </Routes>
     </BrowserRouter>
   
