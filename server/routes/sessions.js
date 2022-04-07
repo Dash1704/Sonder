ffa572a2c3e844808740beab65127938e368f524
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-04-07 12:13:10 +0100

    Enabled the signup page to send an error if user already exists.  Enabled signup button to be functional and lead to signin page.

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const SessionsController = require("../controllers/sessions");
 
 router.post("/login", SessionsController.Create);
-router.delete("/signout", SessionsController.Delete);
 
 module.exports = router
