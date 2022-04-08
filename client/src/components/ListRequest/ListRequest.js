10b11e520ee6e5e97b4de499a0dc57c3d7a4a9f9
Jaroslaw Roman Cudzich <internet.jarek@gmail.com>
2022-04-08 17:04:59 +0100

    Refactored the ListRequest.js file's code.

--- client/src/components/ListRequest/ListRequest.js
+++ client/src/components/ListRequest/ListRequest.js
@@ -10,15 +10,9 @@ const ListRequest = ({ oneRequest }) => {
        <p>{`${oneRequest.text}`}</p> 
        <h5>{`requested by ${oneRequest.userCreatedBy.name}, in ${oneRequest.userCreatedBy.city}`}</h5>
         {
-        oneRequest.active===true && 
-        <RequestHelpButton
-        oneRequest={oneRequest}
-        />
-        } {
-        oneRequest.active===false &&
-        <p>
-            Request is being fulfilled
-        </p>
+        oneRequest.active===true ? 
+        <RequestHelpButton oneRequest={oneRequest}/>:
+        <p>Request is being fulfilled</p>
         }
     </div>
     </>
