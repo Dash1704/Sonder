ff00cc6468975bf2c04f74396cddad9c302e71ef
Francesco <francesco@monzo.com>
2022-04-09 19:12:03 +0200

    edited resolve path for requests from server 2

--- server/app.js
+++ server/app.js
@@ -44,7 +44,7 @@ const port = process.env.PORT || 8080;
 // heroku
 if(process.env.NODE_ENV=="production"){
   app.use(express.static('../client/build'));
-  app.get('*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     console.log("I'm in production now")
   });
