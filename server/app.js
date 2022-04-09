65415660e0419b56502e839a5ae49dd21e9405db
Francesco <francesco@monzo.com>
2022-04-09 22:37:33 +0200

    removed unused get requests from server

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const path = require('path')
 require('dotenv').config();
 
 // Import routes
-const testRouter = require('./routes/test')
+// const testRouter = require('./routes/test')
 const userRouter = require('./routes/users');
 const sessionRouter = require('./routes/sessions');
 const requestRouter = require('./routes/requests')
@@ -27,11 +27,10 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use(express.json())
 app.use(morgan('dev'));
 app.use(cors({origin: true, credentials: true}));
-app.use(express.json())
 
 //routes
 app.use("/sessions", sessionRouter);
-app.use("/", testRouter)
+// app.use("/", testRouter)
 app.use("/users", userRouter);
 app.use("/requests", requestRouter);
 
@@ -43,12 +42,13 @@ const port = process.env.PORT || 8080;
 
 // heroku
 
+if(process.env.NODE_ENV=="production") {
   app.use(express.static("../client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     console.log("I'm in production now")
   });
-
+}
 
 //listener
 const server = app.listen(port, () => console.log(`Server is running on ${port}`))
--- server/routes/requests.js
+++ server/routes/requests.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const RequestController = require("../controllers/requests");
 
 router.post("/new", RequestController.Create)
-router.get('/', RequestController.View)
+// router.get('/', RequestController.View)
 
 module.exports = router
