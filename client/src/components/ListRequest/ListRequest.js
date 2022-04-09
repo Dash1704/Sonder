71b29b0a7f4a49c386c08c2b4c2c6067ddc11185
Eddie Arnold <eddiearnoldz@me.com>
2022-04-09 15:17:36 +0100

    added conditional rendering to donor request fulfilled button

--- client/src/components/ListRequest/ListRequest.js
+++ client/src/components/ListRequest/ListRequest.js
@@ -8,23 +8,36 @@ const ListRequest = ({ oneRequest, allRequests, setAllRequests }) => {
   const { t } = useTranslation();
   const userName = oneRequest.userCreatedBy.name
   const userCity = oneRequest.userCreatedBy.city
-
-    return (
-    <>
-    <div>
-       <p>{`${oneRequest.text}`}</p> 
-       <h5>{t("requested_by_info", {userName, userCity})}</h5>
-        {
-          oneRequest.active===true ? 
-          <RequestHelpButton
-           oneRequest={oneRequest}
-           allRequests={allRequests}
-           setAllRequests={setAllRequests}/> : 
-           <p>Request is being fulfilled</p>
-        }
-    </div>
-    </>
-    )
+  const donor = localStorage.getItem("donor")
+    if(!donor) {
+        return (
+            <>
+            <div>
+               <p>{`${oneRequest.text}`}</p> 
+               <h5>{t("requested_by_info", {userName, userCity})}</h5>   
+            </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <div>
+               <p>{`${oneRequest.text}`}</p> 
+               <h5>{t("requested_by_info", {userName, userCity})}</h5>
+              
+                {
+                oneRequest.active===true ? 
+                <RequestHelpButton
+                    oneRequest={oneRequest}
+                    allRequests={allRequests}
+                    setAllRequests={setAllRequests}/> : 
+                    <p>Request is being fulfilled</p>
+                }
+            </div>
+        </>
+        )
+    }
+    
 }
 
 export default ListRequest
