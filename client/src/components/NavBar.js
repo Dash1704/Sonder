63a597ef51ce36abe70d6bf548d8e33c18d3df66
Eddie Arnold <eddiearnoldz@me.com>
2022-04-09 20:13:31 +0100

    added italian and french to the translator function

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -16,7 +16,43 @@ const languages = [
     name: 'Español',
     country_code: 'ES'
   },
+  {
+    code: 'it',
+    name: 'Italien',
+    country_code: 'IT'
+  },
+  {
+    code: 'fr',
+    name: 'français',
+    country_code: 'FR'
+  },
 ]
+const languageDropdownButton = 
+  <li key="lang">
+      <a 
+      className='dropdown-trigger large material-icons' 
+      href='#' 
+      data-target='dropdown1'
+      onMouseEnter={e => {
+        const inst = M.Dropdown.getInstance(e.target);
+        inst && inst.open();
+        }}
+      >language</a>
+        <ul id='dropdown1' className='dropdown-content'>
+        <li onClick={() => i18next.changeLanguage(languages[0].code)}>
+          <span>{getUnicodeFlagIcon("GB")}</span>{languages[0].name}
+        </li>
+        <li onClick={() => i18next.changeLanguage(languages[1].code)}>
+          <span>{getUnicodeFlagIcon("ES")}</span>{languages[1].name}
+        </li>
+        <li onClick={() => i18next.changeLanguage(languages[2].code)}>
+          <span>{getUnicodeFlagIcon("IT")}</span>{languages[2].name}
+        </li>
+        <li onClick={() => i18next.changeLanguage(languages[3].code)}>
+          <span>{getUnicodeFlagIcon("FR")}</span>{languages[3].name}
+        </li>
+        </ul>
+  </li>
 
 
 const NavBar = () => {
@@ -29,7 +65,6 @@ const NavBar = () => {
       belowOrigin: true
     };
     M.Dropdown.init(dropdowns, options);
-
     }, [])
 
   const { t } = useTranslation();
@@ -43,31 +78,14 @@ const NavBar = () => {
         <li key="aboutus"><Link to="/aboutus">About Us</Link></li>,
         <li key="request"><Link to="/requests">Requests</Link></li>,
         <li key="motherprofile"><Link to="/profile/mother">Profile</Link></li>,
-        <li key="lang">
-        <a 
-        className='dropdown-trigger large material-icons' 
-        href='#' 
-        data-target='dropdown1'
-        onMouseEnter={e => {
-          const inst = M.Dropdown.getInstance(e.target);
-          inst && inst.open();
-          }}
-        >language</a>
-          <ul id='dropdown1' className='dropdown-content'>
-          <li onClick={() => i18next.changeLanguage(languages[0].code)}>
-            <span>{getUnicodeFlagIcon("GB")}</span>{languages[0].name}</li>
-            <li onClick={() => i18next.changeLanguage(languages[1].code)}>
-              <span>{getUnicodeFlagIcon("ES")}</span>{languages[1].name}</li>
-          </ul>
-        </li>,
-        
+        languageDropdownButton,
         <li key="logout">
         <button className="btn waves-effect waves-light #f50057 pink accent-3"
-        onClick={()=> {
-          localStorage.clear()
-          navigate('/login')
-        }}>
-         Log Out 
+          onClick={()=> {
+            localStorage.clear()
+            navigate('/login')
+          }}>
+         {t("logout_navbar")}
         </button>
         </li>
       ]
@@ -77,54 +95,14 @@ const NavBar = () => {
         <li key="aboutus"><Link to="/aboutus">About Us</Link></li>,
         <li key="request"><Link to="/requests">Requests</Link></li>,
         <li key="donorprofile"><Link to="/profile/donor">Profile</Link></li>,
-         <li key="lang">
-        <a 
-        className='dropdown-trigger large material-icons' 
-        href='#' 
-        data-target='dropdown1'
-        onMouseEnter={e => {
-          const inst = M.Dropdown.getInstance(e.target);
-          inst && inst.open();
-          }}
-        >language</a>
-          <ul id='dropdown1' className='dropdown-content'>
-          <li onClick={() => i18next.changeLanguage(languages[0].code)}>
-            <span>{getUnicodeFlagIcon("GB")}</span>{languages[0].name}</li>
-            <li onClick={() => i18next.changeLanguage(languages[1].code)}>
-              <span>{getUnicodeFlagIcon("ES")}</span>{languages[1].name}</li>
-          </ul>
-        </li>,
-        <li key="logout">
-        <button className="btn waves-effect waves-light #f50057 pink accent-3"
-        onClick={()=> {
-          localStorage.clear()
-          navigate('/login')
-        }}>
-        {t( "logout_navbar")}
-        </button>
-        </li>
+        languageDropdownButton,
       ]
     } 
     else {
       return [
         <li key="aboutus"><Link to="/aboutus">{t("about_us_navbar")}</Link></li>,
-        <li key="login"><Link to="/login">{t("login")}</Link></li>, <li key="lang">
-        <a 
-        className='dropdown-trigger large material-icons' 
-        href='#' 
-        data-target='dropdown1'
-        onMouseEnter={e => {
-          const inst = M.Dropdown.getInstance(e.target);
-          inst && inst.open();
-          }}
-        >language</a>
-          <ul id='dropdown1' className='dropdown-content'>
-          <li onClick={() => i18next.changeLanguage(languages[0].code)}>
-            <span>{getUnicodeFlagIcon("GB")}</span>{languages[0].name}</li>
-            <li onClick={() => i18next.changeLanguage(languages[1].code)}>
-              <span>{getUnicodeFlagIcon("ES")}</span>{languages[1].name}</li>
-          </ul>
-        </li>,
+        <li key="login"><Link to="/login">{t("login")}</Link></li>, <li key="lang"></li> ,
+        languageDropdownButton
         
       ]
     }
@@ -132,16 +110,16 @@ const NavBar = () => {
   
   return (
   <div className="container">
-  <nav>
-  <div className="nav-wrapper">
-  <div className="col s12">
-    <Link to="/" className="brand-logo left">Sonder</Link>
-    <ul id="nav-mobile" className="right hide-on-med-and-down">
-      {renderList()}
-    </ul>
-  </div>
-  </div>
-  </nav>
+    <nav>
+      <div className="nav-wrapper">
+        <div className="col s12">
+          <Link to="/" className="brand-logo left">Sonder</Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            {renderList()}
+          </ul>
+        </div>
+      </div>
+    </nav>
   </div>
   )
   }
