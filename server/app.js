18a86f6f588a8a666d2f15ced32fdf864fcc6e17
Francesco <francesco@monzo.com>
2022-04-09 19:58:02 +0200

    removed conditional in app js

--- server/app.js
+++ server/app.js
@@ -42,13 +42,13 @@ app.use("/requests", requestRouter);
 const port = process.env.PORT || 8080;
 
 // heroku
-if(process.env.NODE_ENV=="production"){
+
   app.use(express.static("../client/build"));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     console.log("I'm in production now")
   });
-}
+
 
 //listener
 const server = app.listen(port, () => console.log(`Server is running on ${port}`))
