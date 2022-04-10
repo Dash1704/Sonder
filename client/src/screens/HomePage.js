5f2f35b61f400b3e64a9cf56ee8ae7741bd63ea0
Chloe Stokes <chloeemstokes@gmail.com>
2022-04-10 23:32:15 +0100

    Changed font family and weight on homepage

--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -8,9 +8,9 @@ const HomePage = () => {
   const { t } = useTranslation();
   const navigate = useNavigate()
     return (
-      <div className="homepage">
-        <div className="container">
-          <h2 className="center sonder-logo">{t('welcome_to_sonder')}</h2>
+      // <div className="homepage">
+        <div className="container font-body">
+          <h2 className="center">{t('welcome_to_sonder')}</h2>
           <h3 className="center">{t('helping_mothers')}</h3>
           <div className="section">
             <div className="row">
@@ -29,7 +29,7 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-      </div>
+      // </div>
     )
   }
 
