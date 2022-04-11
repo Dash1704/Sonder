5d561e968a34a5072e985b25f228135c02543215
Chloe Stokes <chloeemstokes@gmail.com>
2022-04-11 17:34:02 +0100

    Worked on the layout on the homepage - getting the grid to be responsive and allow various rows, added an image to the right of the container. Fixed the new line json issue

--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -9,34 +9,37 @@ const HomePage = () => {
   const { t } = useTranslation();
   const navigate = useNavigate()
     return (
-      // <div className="homepage">
-        <div className="container font-body">
-          <div className="row">
+      <div className="font-body">
+        <div className="container">
+        <div className="row homepage-top-divide">
           <div className="col s6">
-          <h1 className="left homepage-header">{t('welcome_to_sonder')}</h1>
-          <h2 className="left homepage-slogan">{t('helping_mothers')}</h2>
-          <h3 className="left homepage-motto">{t('sonder_motto')}</h3>
+            <h1 className="left homepage-header">{t('welcome_to_sonder')}</h1>
+            <h2 className="left homepage-slogan">{t('helping_mothers')}</h2>
+            <h3 className="left homepage-motto">{t('sonder_motto')}</h3>
           </div>
           <div className="col s6">
-          <img className="homepage-image" src={homepage1}></img>
+            <img className="homepage-image" src={homepage1}/>
+            </div>
+        </div>
+        </div>
+      <div className="section">
+        <div className="row homepage-bottom-divide">
+          <div className="container">
+          <div className="col s6">
+          <button className="waves-effect waves-light btn-large btn-block"
+          onClick={()=> {
+            navigate('/signup/mother')
+          }}><i className="large material-icons right">pregnant_woman</i>{t("refugee_mother_button")}</button>
           </div>
-          <div className="section">
-            <div className="row">
-            <div className="col s6">
-            <button className="waves-effect waves-light btn-large btn-block"
-            onClick={()=> {
-              navigate('/signup/mother')
-            }}><i className="large material-icons right">pregnant_woman</i>{t("refugee_mother_button")}</button>
-            </div>
-            <div className="col s6">
-            <button className="waves-effect waves-light btn-large btn-block"
-            onClick={()=> {
-              navigate('/signup/donor')
-            }}><i className="large material-icons right">tag_faces</i>{t("volunteer_button")}</button>
-            </div>
-            </div>
+          <div className="col s6">
+          <button className="waves-effect waves-light btn-large btn-block"
+          onClick={()=> {
+            navigate('/signup/donor')
+          }}><i className="large material-icons right">tag_faces</i>{t("volunteer_button")}</button>
           </div>
         </div>
+        </div>
+      </div>
       </div>
     )
   }
