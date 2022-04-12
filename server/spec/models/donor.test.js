4c65854a75d0e7f47ccf0bb34e4cf7e602c261e7
Eddie Arnold <eddiearnoldz@me.com>
2022-04-12 09:23:19 +0100

    donor spec passing and tested green

--- server/spec/models/donor.test.js
+++ server/spec/models/donor.test.js
@@ -4,24 +4,97 @@ require("../mongodb_helper");
 const Donor = require("../../models/donor");
 
 
-describe('Donor model', () => {
+describe('Mother model', () => {
   beforeEach( async () => {
-    await mongoose.connection.collections.donor.deleteMany({});
+    await mongoose.connection.collections.donors.deleteMany({});
   });
+  describe("Mother schema", () => {
+   
+    it("has a name", async () => {
+      const donor = new Donor({
+        name: "Megan Markle",
+        email: "someone@example.com",
+        password: "password",
+        city: "London"
+      });
+      await donor.save()
+      expect(donor.name).toEqual("Megan Markle");
+    });
 
-  describe('Donor Schema', () => {
+    it("has a password", () => {
+      const donor = new Donor({
+        name: "Megan Markle",
+        email: "someone@example.com",
+        password: "password",
+        city: "London"
+      });
+      expect(donor.password).toEqual("password");
+    });
+
     it("has a city", () => {
       const donor = new Donor({
-        name: "Good Samaritan",
-        email: "anyone@example.com",
+        name: "Megan Markle",
+        email: "someone@example.com",
         password: "password",
+        city: "London"
       });
       expect(donor.city).toEqual("London");
     });
 
+    it("has an email", () => {
+      const donor = new Donor({
+        name: "Megan Markle",
+        email: "someone@example.com",
+        password: "password",
+        city: "London"
+      });
+      expect(donor.email).toEqual("someone@example.com");
+    });
 
+    it("can list all users", async () => {
+      let donors = await Donor.find();
+  
+      expect(donors).toEqual([]);
+    });
+
+    it("can save a user",  async () => {
+      const donor = new Donor({ name: "Bob", email: "someone@example.com", password: "password", city: "London" });
+  
+      await donor.save();
+      const data = await Donor.find()
+  
+      expect(data[0]).toMatchObject({
+        name: "Bob",
+        email: "someone@example.com",
+        password: "password",
+        city: "London"
+        });
+    });
+    it("can't save a user with an email aready signed up", async () => {
+      const donor1 = new Donor({
+        name: "Bob",
+        email: "someone@example.com",
+        password: "password",
+        city: "London"
+      });
+  
+      const donor2 = new Donor({
+        name: "Chris",
+        email: "someone@example.com",
+        password: "password1",
+        city: "London"
+      });
+  
+      await donor1.save()
+      
+      await donor2.save((err) =>{
+        expect(err).toBeTruthy()
+      })
+  
+      const data = await Donor.find() 
+      
+      expect(data.length).toEqual(1)
+    
+    }); 
   })
-
-
-
 })
