4ae3f7398342c44e256ef865c60350704749f56a
Chloe Stokes <chloeemstokes@gmail.com>
2022-04-12 10:00:15 +0100

    Changed homepage collage image again

--- client/src/screens/HomePage.js
+++ client/src/screens/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useNavigate} from 'react-router-dom';
 import { useTranslation } from "react-i18next";
-import homepage from "../images/homepage-image.png"
+import homepage from "../images/homepage-image1.png"
 
 
 const HomePage = () => {
