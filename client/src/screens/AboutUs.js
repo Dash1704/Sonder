9d20b9d00a8acbddb9351b681a927a3d39e74d91
Chloe Stokes <chloeemstokes@gmail.com>
2022-04-13 18:36:25 +0100

    Created grid for content on about us page

--- client/src/screens/AboutUs.js
+++ client/src/screens/AboutUs.js
@@ -5,13 +5,19 @@ import ContactForm from '../components/ContactForm/ContactForm';
 const AboutUs = () => {
   const { t } = useTranslation();
     return (
-        <div className="container aboutus">
-          <h1>{t("welcome_to_sonder")}</h1>
-          <p>{t("about_blurb")}</p>
-          <h3>{t("about_how_it_works")}</h3>
-          <p>{t("about_blurb_2")}</p>
-          <ContactForm/>
+        <div className="container">
+          <div className='row'>
+            <div className='col s7'>
+              <h1>{t("welcome_to_sonder")}</h1>
+              <p>{t("about_blurb")}</p>
+              <h3>{t("about_how_it_works")}</h3>
+              <p>{t("about_blurb_2")}</p>
+            </div>
+          <div className='col s4'>
+            <ContactForm/>
           </div>
+          </div>
+        </div>
     )
   }
 
