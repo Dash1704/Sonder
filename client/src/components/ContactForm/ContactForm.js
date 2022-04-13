1e2e40439c4028307953fe46de8544385fda5d3c
Chloe Stokes <chloeemstokes@gmail.com>
2022-04-13 20:16:31 +0100

    About us page finalised and done

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -37,7 +37,7 @@ const ContactForm = () => {
 
  return (
    <>
-   <h3 className='about-us-works-header'>Get in touch</h3>
+   <h3 className='about-us-works-header'>Get in touch:</h3>
    <div className='card contact-us-form'>
       <div className='contact_form'>
         <form className="col s12" onSubmit={onSubmit}>
@@ -87,7 +87,8 @@ const ContactForm = () => {
         <label className="contact-form-filler-text"htmlFor="message">Your message</label>
         <span className="helper-text" data-error="Required" data-success=""></span>
         </div>
-          <button className="btn waves-effect waves-light call-to-action-button contact-form-button" type="submit" name="action">Send<i className="material-icons right">send</i></button>
+        
+          <button className="btn waves-effect waves-light call-to-action-button contact-form-button top" type="submit" name="action">Send<i className="material-icons right">send</i></button>
         </div>
         </form>
       </div>
